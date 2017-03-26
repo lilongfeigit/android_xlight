aca5795471fb44429df59a99a8546fd1fb8f022b
PeterIJia <jiachenglong1114@163.com>
2017-03-26 12:15:07 +0800

    优化ui

---
+++ app/src/main/java/com/umarbhutta/xlightcompanion/main/SlidingMenuMainActivity.java
@@ -0,0 +1,107 @@
+package com.umarbhutta.xlightcompanion.main;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
+import com.umarbhutta.xlightcompanion.R;
+import com.umarbhutta.xlightcompanion.glance.GlanceFragment;
+
+/**
+ * Created by Administrator
+ */
+
+public class SlidingMenuMainActivity extends SlidingFragmentActivity {
+    private Fragment mContent;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
+//        setTitle("ResponsiveUI");
+
+        // 设置主视图界面
+        setContentView(R.layout.responsive_content_frame);
+
+        initSlidingMenu(savedInstanceState);
+    }
+
+    private void initSlidingMenu(Bundle savedInstanceState) {
+        // check if the content frame contains the menu frame
+        if (findViewById(R.id.menu_frame) == null) {
+            setBehindContentView(R.layout.menu_frame);
+            getSlidingMenu().setSlidingEnabled(true);
+            getSlidingMenu()
+                    .setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+            // show home as up so we can toggle
+//			getActionBar().setDisplayHomeAsUpEnabled(true);
+        } else {
+            // add a dummy view
+            View v = new View(this);
+            setBehindContentView(v);
+            getSlidingMenu().setSlidingEnabled(false);
+            getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
+        }
+
+        // 设置主界面Fragment视图内容
+        if (savedInstanceState != null)
+            mContent = getSupportFragmentManager().getFragment(
+                    savedInstanceState, "mContent");
+        if (mContent == null)
+            mContent = new GlanceFragment();
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.content_frame, mContent).commit();
+
+        // set the Behind View Fragment
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.menu_frame, new MainMenuFragment()).commit();
+
+        // 设置滑动菜单的属性值
+        SlidingMenu sm = getSlidingMenu();
+        sm.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        sm.setShadowWidthRes(R.dimen.shadow_width);
+        sm.setShadowDrawable(R.drawable.shadow);
+        sm.setBehindScrollScale(0.25f);
+        sm.setFadeDegree(0.25f);
+    }
+
+    public void switchContent(final Fragment fragment) {
+        mContent = fragment;
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.content_frame, fragment).commit();
+
+        Handler h = new Handler();
+        h.postDelayed(new Runnable() {
+            public void run() {
+                getSlidingMenu().showContent();
+            }
+        }, 50);
+    }
+
+    public void onBirdPressed(int pos) {
+//        Intent intent = BirdActivity.newInstance(this, pos);
+//        startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                toggle();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        getSupportFragmentManager().putFragment(outState, "mContent", mContent);
+    }
+}
